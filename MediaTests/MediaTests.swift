508e830df032d39e94562d3d02299ae5c3d3dfed
shafia.3techsoft <shafia.3techsoft@gmail.com>
2025-08-08 12:37:51 +0500

    Initial Commit

---
+++ MediaTests/MediaTests.swift
@@ -0,0 +1,17 @@
+//
+//  MediaTests.swift
+//  MediaTests
+//
+//  Created by shafia on 08/08/2025.
+//
+
+import Testing
+@testable import Media
+
+struct MediaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
